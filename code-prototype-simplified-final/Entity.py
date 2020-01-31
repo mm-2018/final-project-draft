66a7f7a6ea388f036ae8f336b2068d1d0e92bbe6
mm-2018 <mmonis2014@gmail.com>
2020-01-31 13:33:50 +0000

    Added simplified and hopefully final version of code - removed fuel/potable water mechanic, added energy mechanic

---
+++ code-prototype-simplified-final/Entity.py
@@ -0,0 +1,42 @@
+import random
+
+class EntityInstance: # use OOP paradigm for each entity type
+    def __init__(self, etType, etDPS, etDist, pr, etHealth, etUCT):
+        # Entity stats
+        self.uniqueRef = random.randint(0, 1024)
+        self.entityType = etType
+        self.entityType = self.entityType.strip().upper()
+        self.entityDist = etDist
+        self.PlayerRef = pr
+        self.entityDPS = etDPS
+        self.entityTTK = self.PlayerRef.health / self.entityDPS
+        self.fuelAmount = 1000 # for fuel - available runtime in seconds
+        self.ammoAmount = 90 # for weapons
+        #self.entityETA = None
+
+        # Entity type check - logic should be executed in separate classes instead.
+        if self.entityDPS == 0: # prevent divide by zero with fauna or hazardous flora
+            self.entityDPS = 0.01
+        else:
+            self.entityDPS = etDPS
+
+        if self.entityType == "FLORA" or self.entityType == "ITEM":
+            self.entityHealth = etHealth
+            self.entityUCT = etUCT
+            self.PlayerRef.energyCost = 0.4 # player has to "wrangle" animal
+
+        if  self.entityType == "FAUNA":
+            self.entityHealth = etHealth
+            self.entityUCT = self.entityTTK
+
+        if self.entityType == "ITEM":
+            self.entityDPS = etDPS
+        print("Stats for " + str(self.entityType) + " " + str(self.uniqueRef) + ": ")
+        print("Distance from player (metres): " + str(self.entityDist))
+
+        if self.entityType == "FAUNA":
+            print("Entity TTK: " + str(self.entityTTK))
+        elif self.entityType == "FLORA" or self.entityType == "ITEM":
+            print("Entity DPS: " + str(self.entityDPS))
+        print("\n")
+        
---
+++ code-prototype-simplified-final/PlayerChar.py
@@ -0,0 +1,86 @@
+class Playerchar:
+    
+    def __init__(self):
+        # Constant values, in seconds       # Notes
+        self.MAXTIMESTARVE = 1600
+        self.TIMEHEALTHRECOVER = 250        # if at least 75 and timeStarve > 0
+        self.STARVATIONDPS = 0.01
+        self.MAXTIMETODIE = 500
+        self.MAXENERGY = 1.0                # maximum energy
+
+        # current stats
+        self.health = 1.0                   # player's current health
+        self.otherEntity = None             # other entity can be unequipped item, flora, fauna. If item equipped, let this affect player accordingly
+        self.timeStarve = 1600              # starvation value
+        self.damagePerSecond = 0.25         # damage player can deal - can change with picked up weapon
+        self.timeDie = self.MAXTIMETODIE
+        self.timeToKillTarget = None        # need to consider variety in Entity class
+        self.useCompletionTime = None
+        #self.scannedEntities[] = None
+        self.calculatedReward = None
+        self.movementSpeed = 3              # used for calculating distance, ETA and related reward values
+        self.negativeReward = 0
+        self.wakefulness = 500
+        self.currentState = {"Health": self.health, "OtherEntity": self.otherEntity, "Wakefulness": self.wakefulness, "DPS": self.damagePerSecond}
+        self.currentAction = None           # for instance, if asleep, attacking, gathering, etc. Maybe use ScanEnv()?
+        #self.predictedState                # same as in currentState, but values are calculation from nextAction
+        #self.nextAction
+        #self.actionItems = {}
+        self.currentEnergy = self.MAXENERGY # current energy - will regenerate after a few seconds.
+        self.energyCost = 0                 # energy cost to complete an action. For instance, killing an animal costs more energy than foraging.
+
+    def CalculateStateValues(self):
+        return None
+
+    def CalculateReward(self, et):
+        #self.proximityReward = 1 / (0.01 * et.entityDist)
+        # calculate reward by add time to arrive and time to complete action
+        self.timeToKillTarget = et.entityHealth / self.damagePerSecond
+        
+        print("Player time to kill target: " + str(self.timeToKillTarget))
+        print("Entity time to kill player: " + str(et.entityTTK))
+
+        if et.entityDPS < 0:
+            et.entityTTK = et.entityTTK*-1 # prevent negative reward
+
+        elif et.entityTTK <= self.timeToKillTarget and et.entityTTK > -0.01:
+            print("Do not engage " + et.entityType + " " + str(et.uniqueRef) + ". It is highly dangerous!")
+            self.negativeReward = self.MAXTIMESTARVE + self.MAXTIMETODIE + (self.STARVATIONDPS * 50)
+            print(self.negativeReward)
+            
+        elif et.entityTTK < -0.01:
+            print("This will heal the player.")
+
+        else:
+            # deduct health by how much entity damage player times how long entity stayed alive.
+            self.health -= et.entityDPS * self.timeToKillTarget # only do this if RLBrain chooses to go forward with this action.
+            self.negativeReward = 0
+
+        self.energyCost = et.entityUCT*0.2# entityTTK = entityUCT
+        if self.energyCost > 1:
+            self.energyCost = 1
+        elif self.energyCost < 0:
+            self.energyCost = self.energyCost * -1
+
+        self.currentEnergy -= self.energyCost
+        print("Energy cost: " + str(self.energyCost))
+        print("Remaining energy: " + str(self.currentEnergy))
+        # if item is deadly and/or will cause damage to player, also consider for deduction. Calculate with number of hits
+        # if action is interrupted, only take damage.
+        # if entity DPS is low, still take damage.
+        # if item will boost player's base damage stat, add to ETA reward
+
+        # calculate reward based on player's remaining time to die
+        # check entity type here
+        # if player is at procGenEnv and is feeling sleepy, and not nearby dangerous fauna, then sleep.
+        # reward with time to die minus time to arrive to location plus time to kill enemy/complete action plus negative reward plus 100 times energy cos
+        self.etaReward = (self.timeDie) - ((et.entityDist / self.movementSpeed) + et.entityUCT + self.negativeReward + (self.energyCost * 100))
+        print("Reward: " + str(self.etaReward))
+        self.currentEnergy = self.MAXENERGY
+    def ScanEnv(self, ets):
+        for i in range (0, len(ets)):
+            print(str(ets[i].entityType) + " " + str(ets[i].uniqueRef) + ":\tDistance: " + str(ets[i].entityDist) + "\tDamage: " + str(ets[i].entityDPS) + "\tUCT: " + str(ets[i].entityUCT) + "\tHealth: " + str(ets[i].entityHealth))
+            self.CalculateReward(ets[i])
+            print("")
+        print("Scanning environment")
+        return None                         # return array of entites as Actions

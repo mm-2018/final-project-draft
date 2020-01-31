2146f505b877eb1f0149a2bce27df9cbb3905625
mm-2018 <mmonis2014@gmail.com>
2020-01-31 17:21:01 +0000

    Attempt to automatically calculate current state/action based on proximity

--- code-prototype-simplified-final/Entity.py
+++ code-prototype-simplified-final/Entity.py
@@ -8,24 +8,24 @@ class EntityInstance: # use OOP paradigm for each entity type
         self.entityType = self.entityType.strip().upper()
         self.entityDist = etDist
         self.PlayerRef = pr
-        self.entityDPS = 1
+        self.entityDPS = etDPS
         self.entityTTK = self.PlayerRef.health / self.entityDPS
         self.fuelAmount = 1000 # for fuel - available runtime in seconds
         self.ammoAmount = 90 # for weapons
         #self.entityETA = None
         self.entityUCT = 0
         self.entityHealth = etHealth
+        self.isCurrentAction = False
 
         # Entity type check - logic should be executed in separate classes instead.
-        if self.entityDPS is not 0: # prevent divide by zero with fauna or hazardous flora
-            self.entityDPS = etDPS
-        else:
+        if self.entityDPS == 0: # prevent divide by zero with fauna or hazardous flora
             self.entityDPS = 0.01
+        else:
+            self.entityDPS = etDPS
 
         if self.entityType == "FLORA" or self.entityType == "ITEM":
             self.entityHealth = etHealth
             self.entityUCT = etUCT
-            self.PlayerRef.energyCost = 0.4 # player has to "wrangle" animal
 
         if  self.entityType == "FAUNA":
             self.entityHealth = etHealth
--- code-prototype-simplified-final/PlayerChar.py
+++ code-prototype-simplified-final/PlayerChar.py
@@ -24,11 +24,12 @@ class Playerchar:
         self.nextState = None
         self.currentAction = None           # for instance, if asleep, attacking, gathering, etc. Maybe use ScanEnv()?
         #self.predictedState                # same as in currentState, but values are calculation from nextAction
+        self.nextAction = None
         #self.nextAction
         #self.actionItems = {}
         self.futureStates = []
         self.policyItem = None              # entities?
-        self.policyItems = []             # make list
+        self.policyItems = []               # make list
         self.currentEnergy = self.MAXENERGY # current energy - will regenerate after a few seconds.
         self.energyCost = 0                 # energy cost to complete an action. For instance, killing an animal costs more energy than foraging.
         self.currentState = {"Health": self.health, "Wakefulness": self.wakefulness, "Energy": self.currentEnergy}
@@ -53,14 +54,19 @@ class Playerchar:
             print(self.negativeReward)
             
         elif et.entityTTK < -0.01:
+            #self.negativeReward -= et.entityTTK*100 # boost reward if entity can heal
             print("This will heal the player.")
 
         else:
             # deduct health by how much entity damage player times how long entity stayed alive.
             self.health -= et.entityDPS * self.timeToKillTarget # only do this if RLBrain chooses to go forward with this action.
-            self.negativeReward = 0
+            self.negativeReward += 0
 
-        self.energyCost = et.entityUCT*0.2# entityTTK = entityUCT
+        # calculate energy cost
+        if et.entityType == "FAUNA":
+            self.energyCost = et.entityTTK*0.2 # player has to "wrangle" animal
+        else:
+            self.energyCost = et.entityUCT*0.05 # entityTTK = entityUCT
         if self.energyCost > 1:
             self.energyCost = 1
         elif self.energyCost < 0:
@@ -68,6 +74,7 @@ class Playerchar:
 
         self.currentEnergy -= self.energyCost
         print("Energy cost: " + str(self.energyCost))
+
         # if item is deadly and/or will cause damage to player, also consider for deduction. Calculate with number of hits
         # if action is interrupted, only take damage.
         # if entity DPS is low, still take damage.
@@ -76,7 +83,8 @@ class Playerchar:
         # calculate reward based on player's remaining time to die
         # check entity type here
         # if player is at procGenEnv and is feeling sleepy, and not nearby dangerous fauna, then sleep.
-        # reward with time to die minus time to arrive to location plus time to kill enemy/complete action plus negative reward plus 100 times energy cos
+        # reward with time to die minus time to arrive to location plus time to kill enemy/complete action plus negative reward plus 100 times energy cost
+
         self.etaReward = (self.timeDie) - ((et.entityDist / self.movementSpeed) + et.entityUCT + self.negativeReward + (self.energyCost * 100))
         print("Reward: " + str(self.etaReward))
         self.nextState = {"Health": self.health, "Wakefulness": self.wakefulness, "Energy": self.currentEnergy}
@@ -88,15 +96,22 @@ class Playerchar:
         self.currentEnergy = self.MAXENERGY
     def ScanEnv(self, ets):
         print("------------SCANNING ENVIRONMENT------------")
+        smallestDistance = 25000
         for i in range (0, len(ets)):
+            if i > 0 and ets[i].entityDist < smallestDistance: # try to set current state for player's current location
+                smallestDistance = ets[i].entityDist
+                self.currentAction = ets[i]
+                #ets[i].isCurrentAction = True
             print(str(ets[i].entityType) + " " + str(ets[i].uniqueRef) + ":\tDistance: " + str(ets[i].entityDist) + "\tDamage: " + str(ets[i].entityDPS) + "\tUCT: " + str(ets[i].entityUCT) + "\tHealth: " + str(ets[i].entityHealth))
+            print("Shortest distance: " + str(smallestDistance))
             self.CalculateReward(ets[i])
             print("")
         return None                         # return array of entites as Actions
 
     def RLBrain(self, cs):
         # code based on that of MorvanZhou's on Github: https://github.com/MorvanZhou/Reinforcement-learning-with-tensorflow
-        print("Current state: " + str(cs) + "\n")
+        print("Current state: " + str(cs))
+        print("Current action: " + str(self.currentAction.entityType) + "\n")
         for i in range (0, len(self.futureStates)):
             print("Candidate state: " + str(self.futureStates[i]) + "\n")
         return None
